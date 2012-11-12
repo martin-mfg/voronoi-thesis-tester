1c763a1b6e53a79503f72e1b11f17d81ac5afb97
martin-mfg <martin-mfg@gmx.de>
2012-11-12 14:41:27 +0100

    added missing edge file

---
+++ C++/QT/Edge.h
@@ -0,0 +1,18 @@
+#include <QtGui>
+
+class Edge {
+public:
+	int x1;
+	int y1;
+	int x2;
+	int y2;
+	QColor color;
+	
+	Edge(int _x1, int _y1, int _x2, int _y2, QColor _color) {
+	 x1=_x1;
+	 y1=_y1;
+	 x2=_x2;
+	 y2=_y2;
+	 color=_color;
+	}
+};
