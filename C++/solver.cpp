5da08faece367eafc2e51a9bef5e3347581efc4f
Jonathan Sharyari <sharyari@gmail.com>
2012-11-17 22:41:27 +0100

    Added a solver in C++/Gurobi. Don't know how to compile it with cmake.

---
+++ C++/solver.cpp
@@ -0,0 +1,108 @@
+// g++ solver.cpp -I /opt/gurobi501/linux32/include/  -L /opt/gurobi501/linux32/lib/ -lgurobi_c++ -L /opt/gurobi501/linux32/lib -lgurobi50
+
+#include "gurobi_c++.h"
+using namespace std;
+
+int main(int   argc, char *argv[]) {
+  try {	
+    GRBEnv env = GRBEnv();
+	vector <vector <int> > M;
+	vector <int> p1;
+	vector <int> p2;
+	vector <int> p3;
+	vector <int> p4;
+	vector <int> p5;
+	vector <int> p6;
+	vector <int> p7;
+	p1.push_back(1);
+	p1.push_back(0);
+	p1.push_back(0);
+	p2.push_back(1);
+	p2.push_back(1);
+	p2.push_back(0);
+	p3.push_back(0);
+	p3.push_back(1);
+	p3.push_back(0);
+	p4.push_back(1);
+	p4.push_back(0);
+	p4.push_back(1);
+	p5.push_back(1);
+	p5.push_back(1);
+	p5.push_back(1);
+	p6.push_back(0);
+	p6.push_back(1);
+	p6.push_back(1);
+	p7.push_back(0);
+	p7.push_back(0);
+	p7.push_back(1);
+	M.push_back(p1);
+	M.push_back(p2);
+	M.push_back(p3);
+	M.push_back(p4);
+	M.push_back(p5);
+	M.push_back(p6);
+	M.push_back(p7);
+
+
+	int numPoints = M.size();
+	int numCircles = M.back().size();
+	GRBVar p[numPoints];
+	double coeff[numPoints];
+        GRBModel model = GRBModel(env);
+	GRBLinExpr expr;
+
+
+
+    // Create variables
+
+	for (int i=0;i<numPoints;i++){
+		p[i] = model.addVar(0.0, 1.0, 0.0, GRB_BINARY);
+		coeff[i] = 1.0;
+		}
+
+	expr.addTerms(coeff, p,numPoints);
+
+    // Integrate new variables
+
+    model.update();
+
+    // Set objective: maximize x + y + 2 z
+
+    model.setObjective(expr, GRB_MINIMIZE);
+
+	for(int i=0;i<numCircles;i++){
+		GRBLinExpr cexpr;
+		double ccoeff[numPoints];
+		for(int j=0;j<numPoints;j++){
+			ccoeff[j] = M[j].back();
+			M[j].pop_back();
+		}
+		cexpr.addTerms(ccoeff,p,numPoints);
+	    model.addConstr(cexpr, GRB_GREATER_EQUAL,1.0);
+	}
+    
+    // Optimize model
+
+    model.optimize();
+for (int i=0;i<numPoints;i++){
+	cout << p[i].get(GRB_DoubleAttr_X) << endl;
+}
+/*
+    cout << x.get(GRB_StringAttr_VarName) << " "
+         << x.get(GRB_DoubleAttr_X) << endl;
+    cout << y.get(GRB_StringAttr_VarName) << " "
+         << y.get(GRB_DoubleAttr_X) << endl;
+    cout << z.get(GRB_StringAttr_VarName) << " "
+         << z.get(GRB_DoubleAttr_X) << endl;
+*/
+    cout << "Obj: " << model.get(GRB_DoubleAttr_ObjVal) << endl;
+
+  } catch(GRBException e) {
+    cout << "Error code = " << e.getErrorCode() << endl;
+    cout << e.getMessage() << endl;
+  } catch(...) {
+    cout << "Exception during optimization" << endl;
+  }
+
+  return 0;
+}
